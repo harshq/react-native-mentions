f6b0e2b20b35635c0011cfcf937100d9e3797145
Harshana Abeyaratne <harshanaa@yohanj.seranet.local>
2017-09-22 17:22:40 +0530

    feat(proptypes): proptypes added

--- src/MentionsTextInput.js
+++ src/MentionsTextInput.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import {
+  Text,
   View,
   Animated,
   TextInput,
   FlatList,
 } from 'react-native';
+import PropTypes from 'prop-types';
 
 export default class MentionsTextInput extends Component {
   constructor() {
@@ -68,7 +70,7 @@ export default class MentionsTextInput extends Component {
 
   identifyKeyword(val) {
     if (this.isTrackingStrated) {
-      const boundary = this.props.triggerLocation === 'new-word-only' ? 'B': '';
+      const boundary = this.props.triggerLocation === 'new-word-only' ? 'B' : '';
       const pattern = new RegExp(`\\${boundary}${this.props.trigger}[a-z0-9_-]+|\\${boundary}${this.props.trigger}`, `gi`);
       const keywordArray = val.match(pattern);
       if (keywordArray && !!keywordArray.length) {
@@ -82,7 +84,7 @@ export default class MentionsTextInput extends Component {
     this.props.onChangeText(val); // pass changed text back
     const lastChar = val.substr(val.length - 1);
     const wordBoundry = (this.props.triggerLocation === 'new-word-only') ? this.previousChar.trim().length === 0 : true;
-    if (lastChar === this.props.trigger && wordBoundry ) {
+    if (lastChar === this.props.trigger && wordBoundry) {
       this.startTracking();
     } else if (lastChar === ' ' && this.state.isTrackingStrated || val === "") {
       this.stopTracking();
@@ -106,8 +108,8 @@ export default class MentionsTextInput extends Component {
             enableEmptySections={true}
             data={this.props.suggestionsData}
             keyExtractor={this.props.keyExtractor}
-            renderItem={(rowData) => { return this.props.renderSuggestionsRow(rowData, this.stopTracking.bind(this)) } }
-            />
+            renderItem={(rowData) => { return this.props.renderSuggestionsRow(rowData, this.stopTracking.bind(this)) }}
+          />
         </Animated.View>
         <TextInput
           {...this.props}
@@ -115,15 +117,47 @@ export default class MentionsTextInput extends Component {
             this.setState({
               textInputHeight: this.props.textInputMinHeight >= event.nativeEvent.contentSize.height ? this.props.textInputMinHeight : event.nativeEvent.contentSize.height + 10,
             });
-          } }
+          }}
           ref={component => this._textInput = component}
           onChangeText={this.onChangeText.bind(this)}
           multiline={true}
           value={this.props.value}
           style={[{ ...this.props.textInputStyle }, { height: Math.min(this.props.textInputMaxHeight, this.state.textInputHeight) }]}
           placeholder={this.props.placeholder ? this.props.placeholder : 'Write a comment...'}
-          />
+        />
       </View>
     )
   }
+}
+
+MentionsTextInput.propTypes = {
+  textInputStyle: TextInput.propTypes.style,
+  suggestionsPanelStyle: View.propTypes.style,
+  loadingComponent: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.element,
+  ]),
+  textInputMinHeight: PropTypes.number,
+  textInputMaxHeight: PropTypes.number,
+  suggestionsPanelHeight: PropTypes.number,
+  trigger: PropTypes.string.isRequired,
+  triggerLocation: PropTypes.oneOf(['new-word-only', 'anywhere']).isRequired,
+  value: PropTypes.string.isRequired,
+  onChangeText: PropTypes.func.isRequired,
+  triggerCallback: PropTypes.func.isRequired,
+  renderSuggestionsRow: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.element,
+  ]).isRequired,
+  suggestionsData: PropTypes.array.isRequired,
+  keyExtractor: PropTypes.func.isRequired
+};
+
+MentionsTextInput.defaultProps = {
+  textInputStyle: {borderColor: '#ebebeb', borderWidth: 1, fontSize: 15},
+  suggestionsPanelStyle: {backgroundColor: 'rgba(100,100,100,0.1)'},
+  loadingComponent: () => <Text>Loading...</Text>,
+  textInputMinHeight: 30,
+  textInputMaxHeight: 80,
+  suggestionsPanelHeight: 45,
 }
