39b34209a08686266181178ab6b2b87b0b0ade61
Jason Zhang <jasoneo016@gmail.com>
2018-03-07 13:57:26 -0800

    rename to test

--- src/MentionsTextInput.js
+++ src/MentionsTextInput.js
@@ -569,7 +569,7 @@ export default class MentionsTextInput extends Component {
             });
           }}
           ref={component => this._textInput = component}
-          accessibilityLabel={ 'chat_text_input_box' }
+          accessibilityLabel={ 'chat_text_input' }
           onChangeText={this.onChangeText.bind(this)}
           onSelectionChange={(event) => { this.onSelectionChange(event.nativeEvent.selection); }}
           disableFullscreenUI={!!this.props.disableFullscreenUI}
